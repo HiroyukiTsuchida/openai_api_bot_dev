d3ba8cec61099298c7d1960fc3d34a026869e01c
Kazukizuki-takahashi <140982011+Kazukizuki-takahashi@users.noreply.github.com>
2023-08-27 14:56:25 +0900

    Add files via upload

--- app_pi_001.py
+++ app_pi_001.py
@@ -61,7 +61,7 @@ if st.session_state["authenticated"]:
                 # Accumulate content and update the bot's response in real time
                 complete_response += content
                 formatted_response = complete_response.replace("\n", "<br>")
-                indented_response = f"<pre style='margin-left: 20px;'>{complete_response}</pre>" # インデントで回答
+                indented_response = "".join([f"<pre style='margin-left: 20px;'>{line}</pre>" for line in complete_response.split('\n')]) # インデントで回答
                 bot_response_placeholder.markdown(indented_response, unsafe_allow_html=True)
 
         # After all chunks are received, add the complete response to the chat history
