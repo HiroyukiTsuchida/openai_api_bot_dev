6fadf38a8b7ab927161e15935eaaf5a920ec8fe9
Kazukizuki-takahashi <140982011+Kazukizuki-takahashi@users.noreply.github.com>
2023-11-27 20:27:04 +0900

    Add files via upload

--- app_pi_001.py
+++ app_pi_001.py
@@ -776,19 +776,19 @@ if st.session_state["authenticated"]:
 #                else:
 #                    st.write("応答テキストがありません。")
 
-                 if generated_text is not None:
-                     # AIモデルの応答から修正後の全文と修正箇所リストを抽出
-                     # 例: 正規表現や文字列処理を使って必要な部分を抽出する
-                     # ここでは具体的な抽出方法は省略し、仮の変数を使用
-                     extracted_bolded_text = "ここに抽出した修正後の全文"
-                     extracted_correction_list = ["ここに抽出した修正箇所リストの項目1", "項目2", ...]
+                if generated_text is not None:
+                    # AIモデルの応答から修正後の全文と修正箇所リストを抽出
+                    # 例: 正規表現や文字列処理を使って必要な部分を抽出する
+                    # ここでは具体的な抽出方法は省略し、仮の変数を使用
+                    extracted_bolded_text = "ここに抽出した修正後の全文"
+                    extracted_correction_list = ["ここに抽出した修正箇所リストの項目1", "項目2", ...]
 
-                     # 抽出したテキストとリストをMarkdown形式で表示
-                     bot_response_placeholder.markdown(extracted_bolded_text)
-                     correction_list_str = "\n".join(extracted_correction_list)
-                     bot_response_placeholder.markdown(correction_list_str)
-                 else:
-                     st.write("応答テキストがありません。")
+                    # 抽出したテキストとリストをMarkdown形式で表示
+                    bot_response_placeholder.markdown(extracted_bolded_text)
+                    correction_list_str = "\n".join(extracted_correction_list)
+                    bot_response_placeholder.markdown(correction_list_str)
+                else:
+                    st.write("応答テキストがありません。")
 
 
 
